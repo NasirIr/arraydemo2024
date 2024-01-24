b0c205163aa86db5094548690d065d25c3a68a59
nasirir <nasirir@sheridan.desire2learn.com>
2024-01-24 14:37:43 -0500

    third commit

---
+++ src/PartTimeStudent.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author irtiza
+ */
+public class PartTimeStudent {
+    
+    private int numOfCourses;
+
+    public PartTimeStudent(int numOfCourses, insId, string sName) {
+    super(sId, sNamne);
+    this.numOfCourses = numOfCourses;       
+    }
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+    
+}
